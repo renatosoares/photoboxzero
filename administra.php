616502d3b99856b048034674200c8c7c06735419
RenatoSoaresPro <renatosoarespro@hotmail.com>
2015-04-24 10:55:58 -0300

    mostrando resultados

--- administra.php
+++ administra.php
@@ -11,7 +11,14 @@ if ($operacao == "incluir") {
 	$resultado = mysqli_query($conexao, $query) or die("Houve um erro na gravação do registro");
 	echo "Imagem inserida com sucesso";
 } elseif ($operacao == "mostrar") {
-	$query
+	$query = "SELECT * FROM posts";
+	$resultado = mysqli_query($conexao, $query);
+	$linhas = mysqli_num_rows($resultado);
+	echo " <h3>Lista de imagens</h3> ";
+	for ($i=0; $i < $linhas; $i++) { 
+		$reg = mysqli_fetch_row($resultado);
+		echo "$reg[0]<br /> $reg[1]<br /> $reg[2]<br /> $reg[3]<br /><br />";
+	}
 }
 
 
