3f80a7c2e13fa8bf1bd98e61a291ad888423949b
Renato Soares <renatosoarespro@hotmail.com>
2015-05-20 18:18:09 -0300

    :construction: listando imagens

--- views/_includes/header-menu.php
+++ views/_includes/header-menu.php
@@ -45,3 +45,43 @@
 </div>  
 
 
+  <section>
+    <article>
+      <h2>Listagem de imagens</h2>
+      <hr />  
+        <div class="container-fluid">
+          <div class="row">
+          <?php
+            // Percorrer a matriz de dados , formatá-lo como HTML
+         
+          // while ($row = mysqli_fetch_array($this->dados) ) { 
+            // Mostrar dados
+         ?>
+        
+          <div class="col-xs-6 col-md-4">
+        <?php
+        //    echo '<p><span>' .                        $row['titulo'] . '</span></p>';
+        //    echo '<p><strong>Categoria:</strong> ' .  $row['categoria'] . '</p>';
+        //    echo '<p><strong>Descrição:</strong> ' .  $row['descricao'] . '</p>';
+        //    if (is_file('views/image_upload/' .       $row['imagem']) && filesize('views/image_upload/' . $row['imagem']) > 0) {
+        //      echo '<div class="col-md-4">
+        //              <img class="img-circle img-responsive" src="' . 'views/image_upload/' . $row['imagem'] . '" alt="Score image" />
+        //            </div>'
+        ?>
+          </div>
+        <?php          
+        //    }
+        //    else {
+        //      echo '<h3>nao existe imagem</h3></div>';
+        //    }
+        //  
+        //  }
+        ?>
+          </div>
+        </div>
+        
+
+    </article>
+  </section>
+
+
--- views/home/home-view.php
+++ views/home/home-view.php
@@ -3,13 +3,13 @@
     <div class="container">
     <div class="row push50">
           <div class="col-md-4 col-md-offset-4 text-center">
-            <h2>Our Work</h2>
+            <h2>Nossos trabalhos</h2>
       <h3>
-      <span class="filter label label-default" data-filter="all">ALL</span>
-  <span class="filter label label-default" data-filter="bw">B&amp;W</span>
-  <span class="filter label label-default" data-filter="nature">Nature</span>
-  <span class="filter label label-default" data-filter="portraits">Portraits</span>
-  <span class="filter label label-default" data-filter="graphics">Portraits</span>
+      <span class="filter label label-default" data-filter="all">Todos</span>
+      <span class="filter label label-default" data-filter="bw">P&amp;B</span>
+      <span class="filter label label-default" data-filter="nature">Natureza</span>
+      <span class="filter label label-default" data-filter="portraits">Retratos</span>
+
   </h3>
             <hr>
           </div>
@@ -18,198 +18,56 @@
     <div class="row">
     
     <div class="gallery">
-    
-          <ul id="Grid" class="gcontainer">
-            <li class="col-md-4 col-sm-4 col-xs-12 mix bw portraits" data-cat="graphics">
-              <a data-toggle="modal" data-target="#portrait1" class="mix-cover">
-                <img class="horizontal" src="views/_img/portrait1-sm.jpg" alt="placeholder">
-                <span class="overlay"><span class="valign"></span><span class="title">Portrait 1</span></span>
-              </a>                
-            </li>
-            <li class="col-md-4 col-sm-4 col-xs-12 mix portraits" data-cat="graphics">
-                <a data-toggle="modal" data-target="#portrait2" class="mix-cover">
-                  <img class="horizontal" src="views/_img/portrait2.jpg" alt="placeholder">
-                  <span class="overlay"><span class="valign"></span><span class="title">Portrait 2</span></span>
-                </a>                
-            </li>
-        <li class="col-md-4 col-sm-4 col-xs-12 mix nature" data-cat="nature">
-                <a data-toggle="modal" data-target="#nature1" class="mix-cover">
-                  <img class="horizontal" src="views/_img/nature1.jpg" alt="placeholder">
-                  <span class="overlay"><span class="valign"></span><span class="title">Nature 1</span></span>
-                </a>
-            </li>
-            <li class="col-md-4 col-sm-4 col-xs-12 mix portraits" data-cat="portraits">
-                <a data-toggle="modal" data-target="#portrait3" class="mix-cover">
-                  <img class="horizontal" src="views/_img/portrait3.jpg" alt="placeholder">
-                  <span class="overlay"><span class="valign"></span><span class="title">Portrait 3</span></span>
-                </a>
-            </li>
-        <li class="col-md-4 col-sm-4 col-xs-12 mix portraits" data-cat="portraits">
-                <a data-toggle="modal" data-target="#portrait5" class="mix-cover">
-                  <img class="horizontal" src="views/_img/portrait5.jpg" alt="placeholder">
-                   <span class="overlay"><span class="valign"></span><span class="title">Portrait 5</span></span>
-                </a>
-            </li>
-        <li class="col-md-4 col-sm-4 col-xs-12 mix nature" data-cat="nature">
-                <a data-toggle="modal" data-target="#nature" class="mix-cover">
-                  <img class="horizontal" src="views/_img/nature.jpg" alt="placeholder">
-                  <span class="overlay"><span class="valign"></span><span class="title">Nature</span></span>
-                </a>
-            </li>
-            <li class="col-md-4 col-sm-4 col-xs-12 mix portraits" data-cat="portrait">
-                <a data-toggle="modal" data-target="#portrait4" class="mix-cover green">
-                  <img class="vertical" src="views/_img/portrait4.jpg" alt="portrait 4">
-                  <span class="overlay"><span class="valign"></span><span class="title">Portrait 4</span></span>           
-                </a>
-            </li>
-        <li class="col-md-4 col-sm-4 col-xs-12 mix bw nature all" data-cat="portrait">
-                <a data-toggle="modal" data-target="#forest" class="mix-cover green">
-                  <img class="vertical" src="views/_img/forest.jpg" alt="Forest">
-                   <span class="overlay"><span class="valign"></span><span class="title">Forest</span></span>                    
-                </a>
-            </li>
-        <li class="col-md-4 col-sm-4 col-xs-12 mix bw nature all" data-cat="bw">
-                <a data-toggle="modal" data-target="#bw1" class="mix-cover green">
-                  <img class="vertical" src="views/_img/bw1.jpg" alt="Black and White">
-                   <span class="overlay"><span class="valign"></span><span class="title">Black &amp; White</span></span>                  
-                </a>
-            </li>
-          </ul>   
-        
+
+      <ul id="Grid" class="gcontainer">
+        <?php 
+          $ir = 0;
+           while ($row2 = mysqli_fetch_array($this->dados) ) {
+             
+          
+        ?>
+        <li class="col-md-4 col-sm-4 col-xs-12 mix bw portraits" data-cat="graphics">
+          <a data-toggle="modal" data-target="#portrait1" class="mix-cover">
+            <img class="horizontal" src="views/image_upload/<?php echo $row2['imagem']; ?>" alt="placeholder">
+            <span class="overlay">
+              <span class="valign">
+              </span>
+              <span class="title">
+                <?php echo $row2['titulo']; ?>
+              </span>
+            </span>
+          </a>                
+        </li>
+        <?php
+          $tituloArray[$ir] = $row2['titulo'];   
+          $imagemArray[$ir] = $row2['imagem'];
+          $ir++;
+          }
+        ?>
+
+
+      </ul>   
+  
 <!-- Load Photo in Modal -->        
-   <div class="modal fade" id="portrait1" tabindex="-1" role="dialog" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-        <h4 class="modal-title text-center">Portrait 1</h4>
-      </div>
-      <div class="modal-body">
-       <img class="thumbnail" alt="Portrait1" src="views/_img/portrait1.jpg"/>
-      </div>
-    </div><!-- /.modal-content -->
-  </div><!-- /.modal-dialog -->
-</div><!-- /.modal -->
-
-  <div class="modal fade" id="portrait2" tabindex="-1" role="dialog"  aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-        <h4 class="modal-title text-center">Portrait 2</h4>
-      </div>
-      <div class="modal-body">
-       <img class="thumbnail" alt="Portrait1" src="views/_img/portrait2.jpg"/>
-      </div>
-    </div><!-- /.modal-content -->
-  </div><!-- /.modal-dialog -->
-</div><!-- /.modal -->
-
-<div class="modal fade" id="portrait3" tabindex="-1" role="dialog" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-        <h4 class="modal-title text-center">Portrait 3</h4>
-      </div>
-      <div class="modal-body">
-       <img class="thumbnail" alt="Portrait1" src="views/_img/portrait3.jpg"/>
-      </div>
-    </div><!-- /.modal-content -->
-  </div><!-- /.modal-dialog -->
-</div><!-- /.modal -->
-
-<div class="modal fade" id="portrait4" tabindex="-1" role="dialog"  aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-        <h4 class="modal-title text-center">Portrait 4</h4>
-      </div>
-      <div class="modal-body">
-       <img class="thumbnail" alt="Portrait1" src="views/_img/portrait4.jpg"/>
-      </div>
-    </div><!-- /.modal-content -->
-  </div><!-- /.modal-dialog -->
-</div><!-- /.modal -->
-
-<div class="modal fade" id="portrait5" tabindex="-1" role="dialog"  aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-        <h4 class="modal-title text-center">Portrait 5</h4>
-      </div>
-      <div class="modal-body">
-       <img class="thumbnail" alt="Portrait1" src="views/_img/portrait5.jpg"/>
-      </div>
-    </div><!-- /.modal-content -->
-  </div><!-- /.modal-dialog -->
-</div><!-- /.modal -->
-
-<div class="modal fade" id="nature" tabindex="-1" role="dialog"  aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-        <h4 class="modal-title text-center">Nature</h4>
-      </div>
-      <div class="modal-body">
-       <img class="thumbnail" alt="nature" src="views/_img/nature.jpg"/>
-      </div>
-    </div><!-- /.modal-content -->
-  </div><!-- /.modal-dialog -->
-</div><!-- /.modal -->
-
-<div class="modal fade" id="nature1" tabindex="-1" role="dialog" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-        <h4 class="modal-title text-center">Nature 1</h4>
-      </div>
-      <div class="modal-body">
-       <img class="thumbnail" alt="nature" src="views/_img/nature1.jpg"/>
-      </div>
-    </div><!-- /.modal-content -->
-  </div><!-- /.modal-dialog -->
-</div><!-- /.modal -->
-
-
-<div class="modal fade" id="forest" tabindex="-1" role="dialog" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-        <h4 class="modal-title text-center">Forest</h4>
-      </div>
-      <div class="modal-body">
-       <img class="thumbnail" alt="forest" src="views/_img/forest.jpg"/>
-      </div>
-    </div><!-- /.modal-content -->
-  </div><!-- /.modal-dialog -->
-</div><!-- /.modal -->
-
-<div class="modal fade" id="bw1" tabindex="-1" role="dialog" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-        <h4 class="modal-title text-center">Black and White</h4>
-      </div>
-      <div class="modal-body">
-       <img class="thumbnail" alt="forest" src="views/_img/bw1.jpg"/>
-      </div>
-    </div><!-- /.modal-content -->
-  </div><!-- /.modal-dialog -->
-</div><!-- /.modal -->
-
+      <div class="modal fade" id="portrait1" tabindex="-1" role="dialog" aria-hidden="true">
+        <div class="modal-dialog">
+          <div class="modal-content">
+            <div class="modal-header">
+              <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+              <h4 class="modal-title text-center"><?php echo $tituloArray[1]; ?></h4>
+            </div>
+            <div class="modal-body">
+             <img class="thumbnail" alt="Portrait1" src="views/image_upload/<?php echo $imagemArray[2]; ?>"/>
+            </div>
+          </div><!-- /.modal-content -->
+        </div><!-- /.modal-dialog -->
+      </div><!-- /.modal -->
 
 <!-- /Load Photo in Modal --> 
     </div>  
-      </div>
     </div>
-      </div>
+    </div>
+    </div>
     <!-- /Portfolio -->   
 
 
