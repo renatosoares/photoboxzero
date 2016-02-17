80657b881f6dadb292275ad764685d3d4bb155d8
Renato Soares <renatosoarespro@hotmail.com>
2016-02-17 15:56:06 -0300

    ajustando variaveis de acordo com o banco

--- models/administra/admin-model.php
+++ models/administra/admin-model.php
@@ -10,13 +10,33 @@ class AdminModel extends MainModel
 	// strings sql
 	private $inserirDados;
 
+	// id
+	// user_id
+	// category_id
+	// title
+	// body
+	// keywords
+	// urlImage
+
+
+
 	// dados para o banco
-	private $titulo;
-	private $categoria;
-	private $descricao;
-	private $imagemUpload;
-	private $imagemUpload_tipo;
-	private $imagemUpload_tamanho;
+	private $userId;
+	private $categoryId;
+	private $title;
+	private $body;
+	private $keywords;
+	private $urlImageUpload;
+	private $urlImageUploadType;
+	private $urlImageUploadSize;
+
+	//
+	// private $titulo;
+	// private $categoria;
+	// private $descricao;
+	// private $imagemUpload;
+	// private $imagemUpload_tipo;
+	// private $imagemUpload_tamanho;
 
 	// apontar caminho para upload da imagem
 	private $apontarCaminho;
@@ -27,38 +47,46 @@ class AdminModel extends MainModel
 	}
 
 	public function getComandosSQL(){
-		$this->inserirDados = "posts (titulo, categoria, descricao, imagem) VALUES ('$this->titulo', '$this->categoria', '$this->descricao', '$this->imagemUpload')";
+		$this->inserirDados = "posts (user_id, category_id, title, body, keywords, urlImage) VALUES ('$this->titulo', '$this->userId', '$this->title', '$this->body', '$this->keywords', '$this->urlImage')";
 		$this->cmdDB->setInserirDadosSQL($this->inserirDados);
 		return $this->cmdDB->getInserirDadosSQL();
 	}
 
-	public function setDadosBanco($titulo, $categoria, $descricao, $imagemUpload, $imagemUpload_tipo, $imagemUpload_tamanho){
-		$this->titulo = $titulo;
-		$this->categoria = $categoria;
-		$this->descricao = $descricao;
-		$this->imagemUpload = $imagemUpload;
-		$this->imagemUpload_tipo = $imagemUpload_tipo;
-		$this->imagemUpload_tamanho = $imagemUpload_tamanho;
+	public function setDadosBanco($userId, $categoryId, $title,  $body, $urlImageUpload, $urlImageUploadType, $urlImageUploadSize){
+		$this->userId = $userId;
+		$this->categoryId = $categoryId;
+		$this->title = $title;
+		$this->body = $body;
+		$this->keywords = $keywords;
+		$this->urlImage = $urlImage;
+		$this->urlImageUploadType = $urlImageUploadType;
+		$this->urlImageUploadSize = $urlImageUploadSize;
 	}
 	public function getDadosBanco($selecaoNum){
 		switch ($selecaoNum) {
 			case 1:
-			$DBanco = $this->titulo;
+			$DBanco = $this->userId;
 			break;
 			case 2:
-			$DBanco = $this->categoria;
+			$DBanco = $this->categoryId;
 			break;
 			case 3:
-			$DBanco = $this->descricao;
+			$DBanco = $this->title;
 			break;
 			case 4:
-			$DBanco = $this->imagemUpload;
+			$DBanco = $this->body;
 			break;
 			case 5:
-			$DBanco = $this->imagemUpload_tipo;
+			$DBanco = $this->keywords;
 			break;
 			case 6:
-			$DBanco = $this->imagemUpload_tamanho;
+			$DBanco = $this->urlImageUpload;
+			break;
+			case 7:
+			$DBanco = $this->urlImageUploadType;
+			break;
+			case 8:
+			$DBanco = $this->urlImageUploadSize;
 			break;
 
 			default:
@@ -68,7 +96,7 @@ class AdminModel extends MainModel
 		return $DBanco;
 	}
 	public function getApontarCaminho(){
-		$this->apontarCaminho = GW_UPLOADPATH . $this->imagemUpload;
+		$this->apontarCaminho = GW_UPLOADPATH . $this->urlImageUpload;
 		return $this->apontarCaminho;
 	}
 
@@ -137,117 +165,121 @@ class AdminModel extends MainModel
 
 		if (isset($_POST['submit'])) {
 			// pega os dados de upload da imagem
-			$this->setDadosBanco($_POST["titulo"], $_POST["categoria"], $_POST["descricao"], $_FILES['imagemUpload']['name'], $_FILES['imagemUpload']['type'], $_FILES['imagemUpload']['size']);
-			echo $this->getDadosBanco(1);
+			/*	foreach ($_POST as $key => $value) {
+			$setPost[$key] = $value;
+		}*/
+		$this->setDadosBanco($_POST["userId"], $_POST["categoryId"], $_POST["title"], $_POST["body"], $_POST["keywords"] , $_FILES['urlImageUpload']['name'], $_FILES['urlImageUpload']['type'], $_FILES['urlImageUpload']['size']);
+		echo $this->getDadosBanco(1);
 
-			if (!empty($this->getDadosBanco(1)) && !empty($this->getDadosBanco(2)) && !empty($this->getDadosBanco(4))) {
-				if ((($this->getDadosBanco(5) == 'image/gif') || ($this->getDadosBanco(5) == 'image/jpeg') || ($this->getDadosBanco(5) == 'image/pjpeg') || ($this->getDadosBanco(5) == 'image/png')) && ($this->getDadosBanco(6) > 0) && ($this->getDadosBanco(6) <= GW_MAXFILESIZE))  {
-					if ($_FILES['imagemUpload']['error'] == 0) {
-						// Mova o arquivo para a pasta de upload
-						//$apontarCaminho = GW_UPLOADPATH . $imagemUpload;
-						// $aptCaminho = $this->getApontarCaminho();
+		if (!empty($this->getDadosBanco(1)) && !empty($this->getDadosBanco(2)) && !empty($this->getDadosBanco(4))) {
+			if ((($this->getDadosBanco(5) == 'image/gif') || ($this->getDadosBanco(5) == 'image/jpeg') || ($this->getDadosBanco(5) == 'image/pjpeg') || ($this->getDadosBanco(5) == 'image/png')) && ($this->getDadosBanco(6) > 0) && ($this->getDadosBanco(6) <= GW_MAXFILESIZE))  {
+				if ($_FILES['urlImageUpload']['error'] == 0) {
+					// Mova o arquivo para a pasta de upload
+					//$apontarCaminho = GW_UPLOADPATH . $imagemUpload;
+					// $aptCaminho = $this->getApontarCaminho();
 
-						if (move_uploaded_file($_FILES['imagemUpload']['tmp_name'], $this->getApontarCaminho())) {
-							// conecte ao banco de dados
-							// $conexaoDB = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME) or die('Não foi possível conectar');
+					if (move_uploaded_file($_FILES['urlImageUpload']['tmp_name'], $this->getApontarCaminho())) {
+						// conecte ao banco de dados
+						// $conexaoDB = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME) or die('Não foi possível conectar');
 
-							// Escreva os dados no banco
-							//$query = "INSERT INTO posts (titulo, categoria, descricao, imagem) VALUES ('$titulo', '$categoria', '$descricao', '$imagemUpload')";
+						// Escreva os dados no banco
+						//$query = "INSERT INTO posts (titulo, categoria, descricao, imagem) VALUES ('$titulo', '$categoria', '$descricao', '$imagemUpload')";
 
-							mysqli_query($this->cmdDB->getConexaoDB(), $this->getComandosSQL());
+						mysqli_query($this->cmdDB->getConexaoDB(), $this->getComandosSQL());
 
-							// Confirmar o sucesso com o usuário
-							echo '<p>Sua imagem foi adicionada</p>';
-							echo '<p><strong>Título:</strong>' . $this->getDadosBanco(1) . '<br />';
-							echo '<strong>Categoria:</strong>' . $this->getDadosBanco(2) . '<br />';
-							echo '<strong>Descrição:</strong>' . $this->getDadosBanco(3) . '<br />';
-							echo '<img src = "' . GW_UPLOADPATH . $this->getDadosBanco(4) . '" alt="Imagen do Banco"/></p>';
-							echo '<p><a href="index.php">&lt;&lt; Voltar </a></p>';
+						// Confirmar o sucesso com o usuário
+						echo '<p>Sua imagem foi adicionada</p>';
+						echo '<p><strong>Categoria:</strong>' . $this->getDadosBanco(2) .
+						echo '<p><strong>Título:</strong>' . $this->getDadosBanco(3) . '<br />';
+						echo '<strong>Descrição:</strong>' . $this->getDadosBanco(4) . '<br />';
+						echo '<strong>Descrição:</strong>' . $this->getDadosBanco(5) . '<br />';
+						echo '<img src = "' . GW_UPLOADPATH . $this->getDadosBanco(6) . '" alt="Imagen do Banco"/></p>';
+						echo '<p><a href="index.php">&lt;&lt; Voltar </a></p>';
 
-							// Apague os dados do formulári
-							$this->setDadosBanco("", "", "", "", "", "");
+						// Apague os dados do formulário
+						$this->setDadosBanco("", "", "", "", "", "");
 
 
-							mysqli_close($this->cmdDB->getConexaoDB());
-						}
-						else{
-							echo '<p>Desculpe, ouve um problema ao fazer o upload da imagem.</p>';
-						}
+						mysqli_close($this->cmdDB->getConexaoDB());
+					}
+					else{
+						echo '<p>Desculpe, ouve um problema ao fazer o upload da imagem.</p>';
 					}
 				}
-				else {
-					echo '<p>A imagem deve ser um GIF, JPEG, ou PNG e não deve ser maios que ' . (GW_MAXFILESIZE / 1024) .' KB em tamanho.</p>';
-				}
+			}
+			else {
+				echo '<p>A imagem deve ser um GIF, JPEG, ou PNG e não deve ser maios que ' . (GW_MAXFILESIZE / 1024) .' KB em tamanho.</p>';
+			}
 
-				// tente excluir o arquivo temporario de imagem
-				@unlink($_FILES['imagemUpload']['tmp_name']);
-			}
-			else{
-				echo '<p>Por favor entre todas as informação para adicionar a imagem.</p>';
-			}
+			// tente excluir o arquivo temporario de imagem
+			@unlink($_FILES['urlImageUpload']['tmp_name']);
+		}
+		else{
+			echo '<p>Por favor entre todas as informação para adicionar a imagem.</p>';
 		}
 	}
-	public function apaga_imagens(){
-		if (isset($_GET['id']) && isset($_GET['titulo']) &&/* isset($_GET['categoria']) && isset($_GET['descricao']) && */isset($_GET['imagem'])) {
-			// pegue os dados do GET
-			$id = $_GET['id'];
-			$titulo = $_GET['titulo'];
-			//	$categoria = $_GET['categoria'];
-			//	$descricao = $_GET['descricao'];
-			$imagemUpload = $_GET['imagem'];
-		} elseif (isset($_POST['id']) && isset($_POST['titulo'])) {
-			$id = $_POST['id'];
-			$titulo = $_POST['titulo'];
-			$imagemUpload = $_POST['imagem'];
-			//$imagemUpload = $_POST['imagemUpload']; //$_FILES['imagemUpload']['name'], $_FILES['imagemUpload']['type']
-		} else {
-			echo '<p>Desculpe, o arquivo não foi especificado para remover.</p>';
-		}
-		if (isset($_POST['submit'])) {
-			if ($_POST['confirm'] == 'Yes'){
-				// delete a imagem do servidor
-				@unlink(GW_UPLOADPATH . $imagemUpload);
-
-				//$conexaoDB = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
-
-
-				// delete as informações do banco
-				//$query = "DELETE FROM posts WHERE id = $id LIMIT 1";
-				//mysqli_query($conexaoDB, $query);
-				//mysqli_close($conexaoDB);
-				//	$this->cmdDB = parent::getBanco();
-				//	$this->dados = parent::getDadosDelete($this->cmdDB);
-				$cmdDB = new AdminController();
-				//$dados =
-				$cmdDB->getDadosDelete($cmdDB->getBanco(), $id);
-
-				mysqli_close($cmdDB->getBanco()->getConexaoDB());
-
-				// Confirmar sucesso com o usuário
-				echo '<p>A imagem com esse título ' .  GW_UPLOADPATH . " " .  $imagemUpload . "<< CAMINHO" . $titulo .', e com essa ID '. $id .', foi removido com sucesso.</p>';
-
-			}else{
-				echo '<p>A imagem não foi removida</p>';
-			}
-		} else if (isset($id) && isset($titulo) /*&& isset($categoria) && isset($descricao)*/) {
-			echo '<p>Tem certeza de que deseja excluir a seguinte imagem?</p>';
-			echo '	<p>
-			<strong>Título: </strong>' . 	$titulo . '<br />
-			<strong>ID: </strong>' . 		$id .'<br />
-			</p>';
-			echo '<form method="post" action="' . HOME_URI . '/administra.php">';
-
-			echo '<input type="radio" name="confirm" value="Yes" /> Yes ';
-			echo '<input type="radio" name="confirm" value="No" checked="checked" /> No <br />';
-
-			echo '<input type="submit" value="Submit" name="submit" />';
-			echo '<input type="hidden" name="id" value="' . 		$id . '" />';
-			echo '<input type="hidden" name="titulo" value="' . 	$titulo . '" />';
-			echo '<input type="hidden" name="imagem" value="' . $imagemUpload . '" />';
-			//echo '<input type="hidden" name="categoria" value="' . 	$categoria . '" />';
-			echo '</form>';
-		}
-
+}
+public function apaga_imagens(){
+	if (isset($_GET['id']) && isset($_GET['title']) &&/* isset($_GET['categoria']) && isset($_GET['descricao']) && */isset($_GET['imagem'])) {
+		// pegue os dados do GET
+		$id = $_GET['id'];
+		$title = $_GET['title'];
+		//	$categoria = $_GET['categoria'];
+		//	$descricao = $_GET['descricao'];
+		$urlImageUpload = $_GET['imagem'];
+	} elseif (isset($_POST['id']) && isset($_POST['title'])) {
+		$id = $_POST['id'];
+		$title = $_POST['title'];
+		$urlImageUpload = $_POST['imagem'];
+		//$imagemUpload = $_POST['imagemUpload']; //$_FILES['imagemUpload']['name'], $_FILES['imagemUpload']['type']
+	} else {
+		echo '<p>Desculpe, o arquivo não foi especificado para remover.</p>';
 	}
+	if (isset($_POST['submit'])) {
+		if ($_POST['confirm'] == 'Yes'){
+			// delete a imagem do servidor
+			@unlink(GW_UPLOADPATH . $urlImageUpload);
+
+			//$conexaoDB = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+
+
+			// delete as informações do banco
+			//$query = "DELETE FROM posts WHERE id = $id LIMIT 1";
+			//mysqli_query($conexaoDB, $query);
+			//mysqli_close($conexaoDB);
+			//	$this->cmdDB = parent::getBanco();
+			//	$this->dados = parent::getDadosDelete($this->cmdDB);
+			$cmdDB = new AdminController();
+			//$dados =
+			$cmdDB->getDadosDelete($cmdDB->getBanco(), $id);
+
+			mysqli_close($cmdDB->getBanco()->getConexaoDB());
+
+			// Confirmar sucesso com o usuário
+			echo '<p>A imagem com esse título ' .  GW_UPLOADPATH . " " .  $urlImageUpload . "<< CAMINHO" . $title .', e com essa ID '. $id .', foi removido com sucesso.</p>';
+
+		}else{
+			echo '<p>A imagem não foi removida</p>';
+		}
+	} else if (isset($id) && isset($title) /*&& isset($categoria) && isset($descricao)*/) {
+		echo '<p>Tem certeza de que deseja excluir a seguinte imagem?</p>';
+		echo '	<p>
+		<strong>Título: </strong>' . 	$title . '<br />
+		<strong>ID: </strong>' . 		$id .'<br />
+		</p>';
+		echo '<form method="post" action="' . HOME_URI . '/administra.php">';
+
+		echo '<input type="radio" name="confirm" value="Yes" /> Yes ';
+		echo '<input type="radio" name="confirm" value="No" checked="checked" /> No <br />';
+
+		echo '<input type="submit" value="Submit" name="submit" />';
+		echo '<input type="hidden" name="id" value="' . 		$id . '" />';
+		echo '<input type="hidden" name="titulo" value="' . 	$title . '" />';
+		echo '<input type="hidden" name="imagem" value="' . $urlImageUpload . '" />';
+		//echo '<input type="hidden" name="categoria" value="' . 	$categoria . '" />';
+		echo '</form>';
+	}
+
+}
 }
 ?>
--- utilitarios/db_data_source.sql
+++ utilitarios/db_data_source.sql
@@ -10,8 +10,40 @@ CREATE TABLE posts (
     imagem VARCHAR(64)
 );
 
--- Para adicionar um novo campo a tabela 
+-- Para adicionar um novo campo a tabela
 ALTER TABLE posts ADD id  INT UNSIGNED AUTO_INCREMENT PRIMARY KEY
 
 -- o comando apaga a tabela e todos os dados contidos nela
 DROP TABLE posts
+
+
+-- novas tabelas do banco
+
+-- tabelas do novo banco para o photobox
+
+CREATE TABLE users (
+    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(50),
+    password VARCHAR(255),
+    role VARCHAR(20),
+    created DATETIME DEFAULT NULL,
+    modified DATETIME DEFAULT NULL
+);
+CREATE TABLE category (
+    id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    created DATETIME,
+    modified DATETIME
+);
+
+CREATE TABLE posts (
+    id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    user_id INT(11) NOT NULL,
+    category_id INT (11),
+    title VARCHAR(255) NOT NULL,
+    body TEXT,
+    keywords TEXT,
+    urlImage varchar(2048),
+    created DATETIME,
+    modified DATETIME
+);
