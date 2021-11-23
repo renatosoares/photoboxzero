9fed4118a0effc7f926f87593dcce6d040dc05de
Renato Soares <renatosoarespro@hotmail.com>
2021-11-23 09:43:26 -0300

    add image background for Hero

--- src/components/atoms/Hero/index.tsx
+++ src/components/atoms/Hero/index.tsx
@@ -1,14 +1,26 @@
 import React from "react";
+import styled, { css } from "styled-components";
 
+import HeroStyles from "./Hero.module.scss";
 import "./styles.scss";
+
+const HeroWrap = styled.div`
+  --hero-bg: url("//picsum.photos/1366/210");
+  ${(props) =>
+    css`
+      cursor: pointer;
+    `}
+`;
 
 const Hero = () => {
   return (
-    <div className="hero">
+    <HeroWrap className={[HeroStyles.hero, "hero"].join(" ")}>
       <div className="container">
-        <h1>Find the perfect asset for your next creative project</h1>
+        <h1 className="title">
+          Find the perfect asset for your next creative project
+        </h1>
       </div>
-    </div>
+    </HeroWrap>
   );
 };
 
