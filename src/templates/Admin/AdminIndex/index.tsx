c270e02e5e932068f62c4f0e1061725c15186bf3
Renato Soares <renatosoarespro@hotmail.com>
2022-05-31 09:04:46 -0300

    refactor: create component ReportHeader

--- src/templates/Admin/AdminIndex/index.tsx
+++ src/templates/Admin/AdminIndex/index.tsx
@@ -1,4 +1,5 @@
 import NavMenu from "components/NavMenu";
+import ReportHeader from "components/ReportHeader";
 
 const AdminIndex = () => {
   return (
@@ -7,42 +8,7 @@ const AdminIndex = () => {
         <div className="row">
           <NavMenu />
           <main className="col-md-9 ms-sm-auto col-lg-10 px-md-4">
-            <div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-              <h1 className="h2">Dashboard</h1>
-              <div className="btn-toolbar mb-2 mb-md-0">
-                <div className="btn-group me-2">
-                  <button
-                    type="button"
-                    className="btn btn-sm btn-outline-secondary"
-                  >
-                    Share
-                  </button>
-                  <button
-                    type="button"
-                    className="btn btn-sm btn-outline-secondary"
-                  >
-                    Export
-                  </button>
-                </div>
-                <button
-                  type="button"
-                  className="btn btn-sm btn-outline-secondary dropdown-toggle"
-                >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="16"
-                    height="16"
-                    fill="currentColor"
-                    className="bi bi-calendar icon me-2"
-                    viewBox="0 0 16 16"
-                  >
-                    <path d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5zM1 4v10a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V4H1z" />
-                  </svg>
-                  This week
-                </button>
-              </div>
-            </div>
-
+            <ReportHeader />
             <h2>Section title</h2>
             <div className="table-responsive">
               <table className="table table-striped table-sm">
