4a72d3bc624f0356ebda5777762f388930cda8c5
Renato Soares <renatosoarespro@hotmail.com>
2022-07-25 09:05:50 -0300

    chore: redirect root to admin

--- next.config.js
+++ next.config.js
@@ -15,6 +15,11 @@ const nextConfig = {
         destination: '/admin/dashboard',
         permanent: true,
       },
+      {
+        source: '/',
+        destination: '/admin/dashboard',
+        permanent: true,
+      },
     ]
   },
 };
