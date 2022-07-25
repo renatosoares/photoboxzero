224544b711e380904ce257d1ed6b5266ceb38f3c
Renato Soares <renatosoarespro@hotmail.com>
2022-07-25 08:56:33 -0300

    refactor: encapsulates components

--- src/layouts/Layout/index.tsx
+++ src/layouts/Layout/index.tsx
@@ -3,6 +3,8 @@
 import Header from "layouts/Header";
 import Footer from "layouts/Footer";
 import { useRouter } from "next/router";
+import NavMenu from "components/NavMenu";
+import ReportHeader from "components/ReportHeader";
 
 type LayoutProps = {
   children: React.ReactNode;
@@ -18,7 +20,16 @@ export default function Layout({ children }: LayoutProps) {
       return (
         <>
           <Header />
-          <main>{children}</main>
+          <div className="container-fluid">
+            <div className="row">
+              <NavMenu />
+              <main className="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+                <ReportHeader />
+                <h2>Section title</h2>
+                {children}
+              </main>
+            </div>
+          </div>
           <Footer />
         </>
       );
--- src/templates/Admin/Dashboard/index.tsx
+++ src/templates/Admin/Dashboard/index.tsx
@@ -1,22 +1,7 @@
 import DataList from "components/DataList";
-import NavMenu from "components/NavMenu";
-import ReportHeader from "components/ReportHeader";
 
 const Dashboard = () => {
-  return (
-    <>
-      <div className="container-fluid">
-        <div className="row">
-          <NavMenu />
-          <main className="col-md-9 ms-sm-auto col-lg-10 px-md-4">
-            <ReportHeader />
-            <h2>Section title</h2>
-            <DataList />
-          </main>
-        </div>
-      </div>
-    </>
-  );
+  return <DataList />;
 };
 
 export default Dashboard;
