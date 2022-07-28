593eb5831bd4fe1503a0c650016c66dc30e49599
Renato Soares <renatosoarespro@hotmail.com>
2022-07-28 09:49:16 -0300

    refactor: add per-page layouts

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,21 +1,25 @@
 import "styles/app.scss";
 import type { AppProps } from "next/app";
 import { SessionProvider } from "next-auth/react";
-import Layout from "layouts/Layout";
 import { useEffect } from "react";
+import type { NextPageWithLayout } from "types/next-page-with-layout";
 
-function App({ Component, pageProps }: AppProps) {
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout;
+};
+
+function App({ Component, pageProps }: AppPropsWithLayout) {
   useEffect(() => {
     if (typeof document !== undefined) {
       require("bootstrap/dist/js/bootstrap.bundle.js");
     }
   }, []);
 
-  return (
+  const getLayout = Component.getLayout ?? ((page) => page);
+
+  return getLayout(
     <SessionProvider session={pageProps.session} refetchInterval={0}>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
+      <Component {...pageProps} />
     </SessionProvider>
   );
 }
--- src/pages/admin/dashboard/index.tsx
+++ src/pages/admin/dashboard/index.tsx
@@ -1,16 +1,23 @@
-import { GetServerSideProps, GetServerSidePropsContext, NextPage } from "next";
+import type { ReactElement } from "react";
+import { GetServerSideProps, GetServerSidePropsContext } from "next";
 import { getCsrfToken, getSession } from "next-auth/react";
 import Dashboard from "templates/Admin/Dashboard";
 import DataTokenProps from "types/data-token-props";
+import type { NextPageWithLayout } from "types/next-page-with-layout";
+import Layout from "layouts/Layout";
 
 type DashboardPageProps = {
   csrfToken: string;
 };
 
-const DashboardPage: NextPage<DashboardPageProps> = (
+const DashboardPage: NextPageWithLayout<DashboardPageProps> = (
   props: DashboardPageProps
 ) => {
   return <Dashboard />;
+};
+
+DashboardPage.getLayout = function getLayout(page: ReactElement) {
+  return <Layout>{page}</Layout>;
 };
 
 export const getServerSideProps: GetServerSideProps = async (
